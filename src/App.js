74ef71978bb2a761633bd34879e04ff89e96c6b7
Shengwei LI <51034296+sw-li@users.noreply.github.com>
2023-02-25 16:47:36 +0100

    it5 done

--- src/App.js
+++ src/App.js
@@ -3,17 +3,34 @@ import IdCard from "./components/IdCard"
 import Greetings from "./components/Greetings"
 import Random from "./components/Random"
 import BoxColor from './components/BoxColor';
+import CreditCard from './components/CreditCard';
 import jsonData from "./data/berlin.json"
+
+
+const aCard = {  
+  type:"Master",
+  number:"0123456789018845",
+  expirationMonth:3,
+  expirationYear:2021,
+  bank:"BNP",
+  owner:"Maxence Bouret",
+  bgColor:"gold",
+  color:"white" }
+
+
+
+
 function App() {
   const copyData = [...jsonData]
-  const idCards = copyData.map(person => <IdCard person={person} ></IdCard> )
+  /* const idCards = copyData.map(person => <IdCard person={person} ></IdCard> ) */
   return (
     <div className="App">
       
       {/* {idCards} */}
       {/* <Greetings lang="en" message="Shengwei"></Greetings> */}
       {/* <Random min={1} max={20} ></Random> */}
-      <BoxColor r="250" g="0" b="0"></BoxColor>
+      {/* <BoxColor r="250" g="0" b="0"></BoxColor> */}
+      <CreditCard card={aCard}></CreditCard>
     </div>
   );
 }
--- src/components/CreditCard.jsx
+++ src/components/CreditCard.jsx
@@ -1,16 +1,18 @@
-function CreditCard(props){
-    const {card} = props
+
+
+function CreditCard({card}){
+
     return (
-        <div className="CreditCard" style={{backgroundColor:card.bgColor}}>
+        <div className="CreditCard" style={{backgroundColor:card.bgColor, color:card.color}}>
             <div className="cardType txtAlignRight">
-                <img src={card.type==="Master Card"? "../assets/images/master-card.svg": "../assets/images/visa.png"} alt="card type" />
+                <img className="miniLogo" src={card.type === "Visa"? "./visa.Png":"./master-card.svg"} alt="card type" />
             </div>
             <div className="cardNumber" style={{backgroundColor:card.bgColor}}>
-                <h1><span className="bolderTxt txtAlignCenter">···· ···· ···· ···· </span>{card.number.splice(0,-4)} </h1>
+                <h1><span className="bolderTxt txtAlignCenter">···· ···· ···· ···· </span>{card.number%10000} </h1>
             </div>
                 
             <div className="creditCardInfo txtAlignLeft">
-                <h3>Expires: {card.expirationMonth}/{card.expirationYear}  {card.bank} </h3>
+                <h3>Expires: {card.expirationMonth}/{card.expirationYear}&nbsp {card.bank} </h3>
                 <h3>{card.owner}</h3>
             </div>
         </div>
